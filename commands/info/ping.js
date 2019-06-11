0d543584dc1ccc0f323d86c9c9ec2c3f8de19d06
GitPaulo <paulo.santos98@gmail.com>
2019-06-11 22:03:34 +0100

    Standardize ping.js (forgot :b)

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,7 +3,7 @@ const mathematics   = xrequire('./modules/utility/mathematics');
 const messageEmbeds = xrequire('./modules/utility/messageEmbeds');
 
 class Ping extends Command {
-    constructor(type) { // type = root folder name (passed on by command loader)
+    constructor (type) { // type = root folder name (passed on by command loader)
         super(type, {
                 name: 'ping',
                 aliases: ['pingpong', 'latency'],
@@ -12,10 +12,10 @@ class Ping extends Command {
                 examples: ['ping'],
                 throttling: 2000,
                 args: [],
-                embed:  {
-                    color:  0xFF0000,
-                    title:  `Ping...`,
-                    image:  './assets/media/images/commands/Ping.gif',
+                embed: {
+                    color: 0xFF0000,
+                    title: `Ping...`,
+                    image: './assets/media/images/commands/Ping.gif'
                 }
             }
         );
@@ -26,27 +26,27 @@ class Ping extends Command {
             `OI OI m8!!`,
             `Pinging...`,
             `This wont take long...`,
-            `Ping request received....`,
+            `Ping request received....`
         ];
     }
 
     // eslint-disable-next-line no-unused-vars
-    async validate(message) {
+    async validate (message) {
         return true; // if true execute() will run
     }
 
-    async execute(message) {
+    async execute (message) {
         let ping       = mathematics.round(message.client.ping, 2);
         let preMessage = await message.channel.send(this.phrases[Math.floor(Math.random() * this.phrases.length)]);
         let reply      = messageEmbeds.reply({
             replyeeMessage: message,
-            title: "Pong!",
-            fields: [ 
+            title: 'Pong!',
+            fields: [
                 { name: 'Server Latency', value: `${preMessage.createdTimestamp - message.createdTimestamp}ms` },
-                { name: 'API Latency',    value:  `${ping}ms` }
+                { name: 'API Latency',    value: `${ping}ms` }
             ]
         });
-        
+
         await message.channel.send(reply);
     }
 }
