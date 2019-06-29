bcf49320d9999f5c66370030e5ab1a909755600e
Carlos Gomes <carlosmiguel.gomes@live.com.pt>
2019-06-29 20:06:30 +0100

    code style

--- commands/discord/fun/connect4.js
+++ commands/discord/fun/connect4.js
@@ -8,7 +8,7 @@ class Connect4 extends DiscordCommand {
         super(commandDefinition);
         this.boardWidth = 7;
         this.boardHeight = 6;
-        this.emojiPlays = []; 
+        this.emojiPlays = [];
         for (let i = 0; i < this.boardWidth; i++) {
             this.emojiPlays.push(i + numberEmojiSuffix);
         }
@@ -36,10 +36,10 @@ class Connect4 extends DiscordCommand {
         let gameMessage = null;
         let turnMessage = null;
 
-        await message.channel.send('```' + board.toString() + '```').then(async boardMessage => gameMessage = boardMessage);
+        await message.channel.send('```' + board.toString() + '```').then(async function(boardMessage) {gameMessage = boardMessage});
         await this.setControls(gameMessage, this.emojiPlays);
-        await message.channel.send(`<@${players[turn-1].id}>'s turn`).then(async m => turnMessage = m);
-        while(!win && !draw && !exit) {
+        await message.channel.send(`<@${players[turn - 1].id}>'s turn`).then(async function(m) {turnMessage = m});
+        while (!win && !draw && !exit) {
             let move = await this.getMove(turn, board, players, gameMessage);
             try {
                 let state = board.makeMoveAndCheckWin(turn, move);
@@ -50,8 +50,8 @@ class Connect4 extends DiscordCommand {
                 console.log(e);
                 exit = true;
             }
-            if(!win && !draw) turn = (turn === 1 ? 2 : 1);
-            turnMessage.edit(`<@${players[turn-1].id}>'s turn`);
+            if (!win && !draw) turn = (turn === 1 ? 2 : 1);
+            turnMessage.edit(`<@${players[turn - 1].id}>'s turn`);
         }
         await gameMessage.edit('```' + board.toString() + '```');
         if (win) turnMessage.edit(`<@${players[turn - 1].id}> WINS!`);
@@ -62,8 +62,7 @@ class Connect4 extends DiscordCommand {
         for (let emoji of emojis) {
             try {
                 await message.react(emoji);
-            }
-            catch (error) {
+            } catch (error) {
                 console.log(error);
                 return;
             }
@@ -72,21 +71,20 @@ class Connect4 extends DiscordCommand {
 
     async getMove (turn, board, players, gameMessage) {
         let move = -1;
-        if(players[turn-1].bot) {
+        if (players[turn - 1].bot) {
             move = MCTS(board, turn);
         } else {
             let filter = (reaction, user) => {
                 return players[turn - 1].id === user.id && this.emojiPlays.includes(reaction.emoji.identifier);
             };
-            await gameMessage.awaitReactions(filter, { max: 1})
+            await gameMessage.awaitReactions(filter, { max: 1 })
                     .then(collected => {
                         let reaction = collected.first();
-                        move = parseInt(reaction.emoji.identifier.slice(0, 1));
-                        reaction.remove(players[turn-1]);
+                        move = parseInt(reaction.emoji.identifier.slice(0, 1), 10);
+                        reaction.remove(players[turn - 1]);
             });
         }
         return move;
     }
-
 }
 module.exports = Connect4;
