6995c28046e72d9f3fa2da617e1cf7e81c85313d
GitPaulo <paulo.santos98@gmail.com>
2019-07-30 03:06:17 +0100

    Stupid mistakes man... ForsenCD

--- webhooks/restart.js
+++ webhooks/restart.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 const logger   = xrequire('./managers/LogManager').getInstance();
 
-const command = (global.isLinux ? 'sudo ' : '') + 'npm run restart';
+const command = (global.isLinux ? 'sudo ' : '') + 'npm run production:restart';
 
 module.exports = (vulcan, request, response) =>
     exec(command, (err, stdout, stderr) =>
