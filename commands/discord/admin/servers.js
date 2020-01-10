355e51f1937d9df4ab797888281c8bf68945cccc
GitPaulo <paulo.santos98@gmail.com>
2020-01-10 06:32:51 +0000

    Fix small bug with servers command.

--- commands/discord/admin/servers.js
+++ commands/discord/admin/servers.js
@@ -10,7 +10,16 @@ servers.execute = async (message) => message.channel.send(messageEmbeds.reply(
             + `\n=========== [Server Listing (ALL)] ===========\n\n`
             + `${Array.from(message.client.servers.entries()).map((entry) => {
                 let guild = message.client.guilds.get(entry[0]);
-                let date  = new Date(entry[1]).toLocaleDateString();
+
+                if (!guild) {
+                    guild = {
+                        name      : '(Unknown)',
+                        id        : entry[0],
+                        authorised: '[A - But this guild has been un-networked!]'
+                    };
+                }
+
+                let date = new Date(entry[1]).toLocaleDateString();
 
                 return `- ${guild.name}(${guild.id})[${guild.authorised && 'A' || 'U'}] joined '${date}'\n`;
             }).join('\n')}\n\`\`\``
